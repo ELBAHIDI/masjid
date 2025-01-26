ec26245f46fd7ef1f69b660496ab7aa854c71462
hassan <elbahidi@gmail.com>
2025-01-26 00:29:07 -0500

    Remove All Docusaurus logo from my main page  and also all otther pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,12 +1,37 @@
 module.exports = {
+  title: 'Islam Gate',
+  tagline: 'Your Gateway to Islamic Knowledge',
   // ... other config
   themeConfig: {
     navbar: {
-      // Hide default navbar if you want to completely replace it
-      style: 'dark',
+      // Completely disable default navbar
       hideOnScroll: true,
-      items: [], // Empty to use custom navbar
+      style: 'dark',
+      items: [],
+      // Explicitly remove logo
+      logo: null,
+      title: null,
     },
-    // ... other theme config
+    // Disable footer if you don't want it
+    footer: {
+      style: 'dark',
+      links: [],
+      copyright: 'Copyright © Islam Gate',
+    },
   },
+  // Disable default pages
+  plugins: [],
+  themes: [],
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        blog: false, // Disable default blog
+        docs: false, // Disable default docs
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
 }; 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,15 @@
 import React from 'react';
-import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
-
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header>
-      <HomepageFeatures />
-    </header>
-  );
-}
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      title="Islam Gate"
+      description="Your Gateway to Islamic Knowledge and Spirituality">
+      <main>
+        <HomepageFeatures />
+      </main>
     </Layout>
   );
 }
