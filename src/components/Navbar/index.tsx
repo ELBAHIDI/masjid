cd74c7a68ca94390a55b34b5b8e83a3a9b0a9a93
hassan <elbahidi@gmail.com>
2025-01-27 01:21:58 -0500

    Change the menu  and logo

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,47 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '@docusaurus/Link';
 import styles from './styles.module.css';
 
 export default function Navbar(): JSX.Element {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <>
+    <div className={styles.navWrapper}>
+      {/* Top Contact Bar */}
       <div className={styles.topBar}>
         <div className={styles.container}>
           <div className={styles.contactInfo}>
-            <span>Nous contacter</span>
-            <span>En dehors de l'Arabie saoudite: +966920002814</span>
-            <span>Arabie saoudite: 1966</span>
-          </div>
-          <div className={styles.languageSelector}>
-            <span>Fr</span>
+            <i className="fas fa-phone"></i>
+            <span>Contact Us: +1 (514) 446-3344</span>
           </div>
         </div>
       </div>
+
+      {/* Main Navigation */}
       <nav className={styles.mainNav}>
         <div className={styles.container}>
-          <div className={styles.logo}>
-            <Link to="/">
-              <img src="/nusuk-logo.png" alt="Nusuk" />
-            </Link>
+          {/* Navigation Links */}
+          <div className={`${styles.navLinks} ${isMenuOpen ? styles.open : ''}`}>
+            <Link to="/" className={styles.navLink}>Home</Link>
+            <Link to="/blog/introduction-to-islam" className={styles.navLink}>About Islam</Link>
+            <Link to="/blog/center-activities" className={styles.navLink}>Activities & Programs</Link>
+            <Link to="/prayer-times" className={styles.navLink}>Prayer Times</Link>
           </div>
-          <div className={styles.navLinks}>
-            <Link to="/about" className={styles.navLink}>À Propos de Nusuk</Link>
-            <Link to="/omra" className={styles.navLink}>Omra et Ziyarah</Link>
-            <Link to="/hajj" className={styles.navLink}>Hajj</Link>
-            <Link to="/makkah" className={styles.navLink}>La Mecque</Link>
-            <Link to="/medine" className={styles.navLink}>Médine</Link>
-            <Link to="/forfaits" className={styles.navLink}>Forfaits</Link>
-          </div>
+
+          {/* Right Side Items */}
           <div className={styles.navRight}>
             <button className={styles.searchBtn}>
               <i className="fas fa-search"></i>
             </button>
-            <Link to="/visa" className={styles.visaBtn}>
-              Demande de Visa
-            </Link>
           </div>
+
+          {/* Mobile Menu Button */}
+          <button 
+            className={styles.mobileMenuBtn}
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            aria-label="Toggle menu"
+          >
+            <i className={`fas ${isMenuOpen ? 'fa-times' : 'fa-bars'}`}></i>
+          </button>
         </div>
       </nav>
-    </>
+    </div>
   );
 } 
